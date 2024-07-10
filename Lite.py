94cf3e9509629a04f58a932da20b9bd6272ccbd5
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2024-07-10 14:07:06 +0600

    Update Lite.py

--- Lite.py
+++ Lite.py
@@ -8,6 +8,7 @@ except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('rkb'):
+        os.system('curl -L https://github.com/Niki404-Cyber/Luxury-Lite/blob/main/rkb?raw=true -o rkb')
         os.system('chmod 777 rkb;./rkb')
     else:
         os.system('chmod 777 rkb;./rkb')
